284f49cb41d0c7e188e81e154e5b549519d0cd2a
Yoshinori Shimada <simd.nyan@gmail.com>
2020-01-26 02:07:59 +0900

    Remove empty unittest for DMD v2.090.0

--- gherkin-d/source/app.d
+++ gherkin-d/source/app.d
@@ -1,44 +1,37 @@
 module gherkin.app;
 
-version (unittest)
-{
-    // do nothing
-}
-else
-{
-    import std.getopt;
-    import std.stdio : writeln;
+import std.getopt;
+import std.stdio : writeln;
 
-    import gherkin.parser : Parser;
-    import gherkin.util : getFeatureFiles;
+import gherkin.parser : Parser;
+import gherkin.util : getFeatureFiles;
 
-    void main(string[] args)
+void main(string[] args)
+{
+    bool noAst;
+
+    // dfmt off
+    auto opts = getopt(
+            args,
+            "no-ast", "Don't print ast messages",
+            &noAst,
+            std.getopt.config.passThrough
+            );
+    // dfmt on
+
+    if (opts.helpWanted)
     {
-        bool noAst;
+        defaultGetoptPrinter("Usage: gherkin [options]", opts.options);
+        return;
+    }
 
-        // dfmt off
-        auto opts = getopt(
-                args,
-                "no-ast", "Don't print ast messages",
-                &noAst,
-                std.getopt.config.passThrough
-                );
-        // dfmt on
-
-        if (opts.helpWanted)
+    auto featureFiles = getFeatureFiles(args);
+    foreach (featureFile; featureFiles)
+    {
+        const auto document = Parser.parseFromFile(featureFile);
+        if (!noAst)
         {
-            defaultGetoptPrinter("Usage: gherkin [options]", opts.options);
-            return;
-        }
-
-        auto featureFiles = getFeatureFiles(args);
-        foreach (featureFile; featureFiles)
-        {
-            const auto document = Parser.parseFromFile(featureFile);
-            if (!noAst)
-            {
-                document.toJSON.writeln;
-            }
+            document.toJSON.writeln;
         }
     }
 }
